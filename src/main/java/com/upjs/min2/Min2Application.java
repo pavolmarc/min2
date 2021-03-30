9d05c003cb577478a0578282aca8f975d66094fd
pavol.marc <pavol.marc@gmail.com>
2021-03-30 09:00:45 +0200

    commit first spring

---
+++ src/main/java/com/upjs/min2/Min2Application.java
@@ -0,0 +1,13 @@
+package com.upjs.min2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Min2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Min2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/upjs/min2/Min2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upjs.min2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Min2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
