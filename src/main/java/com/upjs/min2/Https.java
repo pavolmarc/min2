9f1e722bd0c79d042427d4f3edce5c05b7295192
pavol.marc <pavol.marc@gmail.com>
2021-04-06 08:25:17 +0200

    cpommit

---
+++ src/main/java/com/upjs/min2/Https.java
@@ -0,0 +1,16 @@
+package com.upjs.min2;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Https{
+
+   int ahoj;
+
+}
