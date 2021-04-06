ed59f256600aef941ebadd94340360bbb44d8fe5
pleskova <petra.leskova15@gmail.com>
2021-04-06 09:25:16 +0200

    Front - axios

--- min2-front/src/App.js
+++ min2-front/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import HttpGet from './http.js'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <HttpGet />
     </div>
   );
 }
