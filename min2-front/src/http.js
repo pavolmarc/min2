1aacfdfed322ca43c23193b35b10a246b9bc3d4a
pavol.marc <pavol.marc@gmail.com>
2021-04-06 09:33:59 +0200

    non working axios

--- min2-front/src/http.js
+++ min2-front/src/http.js
@@ -1,15 +1,21 @@
 import axios from 'axios'
- 
+import React,{useState} from 'react'
+
 const HttpGet = () => {
   console.log('The link was clicked.')
- 
+  const [data, setData] = useState("data1")
   axios
     .get(
       'http://localhost:8080/hello-world'
     )
     .then((res) => {
-      const data = res.data
+      setData(res.data)
       console.log('DATA is' + data)
     })
+    return(
+    <div>
+    {data}
+    </div>
+    )
 }
  export default HttpGet
